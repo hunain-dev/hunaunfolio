3265e2bc01543edde6c616ffddcb57761cc28e40
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-02 01:20:53 +0500

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,41 +8,60 @@ import Button from './Ui/Button';
   };
 const Header = () => {
   return (
-    <header className=" fixed top-0 left-1/2 -translate-x-1/2 h-[9vh] w-[93%] flex items-center justify-between z-999 mt-[1vw] lg:opacity-100 opacity-0">
+<header
+  className="
+    fixed 
+    left-1/2 -translate-x-1/2 
+    lg:h-[9vh] lg:w-[93%] 
+    flex items-center justify-between 
+    z-999 mt-[1vw] lg:opacity-100
+    w-[100%]
+
+    top-0
+
+    /* Mobile ke liye bottom se */
+    sm:top-0
+    top-auto bottom-2
+  "
+>
       <div className="
       flex items-center justify-center 
-      gap-[2.2vw] 
+      lg:gap-[2.2vw] 
+      gap-[5vw]
       bg-[#f7f4f3] 
-      rounded-[3vw] 
-      px-[4vw] py-[1vw]
+      lg:rounded-[3vw]
+      rounded-[5vw] 
+      lg:px-[4vw] lg:py-[1vw]
+      
+      px-[6.5vw] py-[3vw]
       shadow-[1px_1px_14px_rgba(29,28,28,0.5)]
       font-[Satoshi]">
       
  <a href="#home">
   <Textanimation
     text="Home"
-    classname="text-black text-[1.2vw] 2xl:text-[1.3vw]"
+    classname="text-black lg:text-[1.2vw] 2xl:text-[1.3vw] text-[3.5vw] "
   />
 </a>
 <a href='#aboutus'>
   <Textanimation
-    text="About us"
-    classname="text-black text-[1.2vw] 2xl:text-[1.3vw]"
+    text="About"
+    classname="text-black text-[1.2vw] 2xl:text-[1.3vw] text-[3.5vw] "
   />
 </a>
   
   
 <a href="#stack">
   <Textanimation
-    text="Stack"
-    classname="text-black text-[1.2vw] 2xl:text-[1.3vw]"
+    text="Stack" 
+    classname="text-black text-[1.2vw] 2xl:text-[1.3vw] text-[3.5vw]"
   />
 </a>
   
 <a href="#services">
   <Textanimation
     text="Services"
-    classname="text-black text-[1.2vw] 2xl:text-[1.3vw]"
+    classname="text-black text-[1.2vw] 2xl:text-[1.3vw] text-[3.5vw]"
   />
 </a>
   
@@ -50,14 +69,14 @@ const Header = () => {
 <a href="#project">
   <Textanimation
     text="Project"
-    classname="text-black text-[1.2vw] 2xl:text-[1.3vw]"
+    classname="text-black text-[1.2vw] 2xl:text-[1.3vw] text-[3.5vw]"
   />
 </a>
   
 <a href="#contact">
   <Textanimation
     text="Contact"
-    classname="text-black text-[1.2vw] 2xl:text-[1.3vw]"
+    classname="text-black text-[1.2vw] 2xl:text-[1.3vw] text-[3.5vw]"
   />
 </a>
   
@@ -65,7 +84,7 @@ const Header = () => {
   
     {/* RIGHT BUTTON */}
     <div className="flex items-center justify-end h-full">
-    <Button   btntext="Resume" onclick={downloadResume}  classname="uppercase px-5 py-2 bg-[#FFD900]"  />
+    <Button   btntext="Resume" onclick={downloadResume}  classname="lg:opacity-100 md:opacity-100 opacity-0 uppercase px-5 py-2 bg-[#FFD900]"  />
 
     </div>
   
