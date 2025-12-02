575d98e6391e11821d0a384ee4c8659990e85ee5
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-02 22:20:52 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import TechStack from './Sections/skills/TechStack'
 import Ourservices from './Sections/services/OurServices'
 import Projects from './Sections/projects/Projects'
-import Aboutus from "./sections/about/Aboutus"
+import Aboutus from "./Sections/about/Aboutus"
 import Home from "./Sections/home/Home"
 const App = () => {
   return (
