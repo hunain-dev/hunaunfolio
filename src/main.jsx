1b0843315c143860a421fe679bb41a7a57e7da60
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-02 20:34:22 +0500

    fix: file path and updates

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Cursor from './components/Cursor.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
 import SmoothScrolling from './hooks/SmoothScrolling.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Chilliman from './components/ChilliMan.jsx'
 import Loader from './components/Loader.jsx'
+import Cursor from './components/Cursor.jsx'
+import Chilliman from './components/ChilliMan.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 
 createRoot(document.getElementById('root')).render(
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')).render(
       <BrowserRouter>
       <Loader/>
       <Cursor/>
-      <Chilliman />
+      <Chilliman/>
 
       <Header/>
     <App/>
