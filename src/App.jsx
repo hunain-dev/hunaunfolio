a1bf2bd0b23dc8230f1cfd045ff4216ea4f063d6
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-02 22:15:39 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./Sections/Home/Home"
 import TechStack from './Sections/skills/TechStack'
 import Ourservices from './Sections/services/OurServices'
 import Projects from './Sections/projects/Projects'
-import Aboutus from './sections/About/Aboutus'
+import Aboutus from "./sections/about/Aboutus"
 const App = () => {
   return (
     <div className='lg:pt-31 pt-13 w-full '>
