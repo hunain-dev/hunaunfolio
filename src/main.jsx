8c736de3cda7b33bfeb63790d1f5980c61689550
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-02 20:28:50 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Loader from './components/Loader.jsx'
 import Cursor from './components/Cursor.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import SmoothScrolling from './hooks/SmoothScrolling.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Chilliman from './components/ChilliMan.jsx'
+import Loader from './components/Loader.jsx'
 
 
 createRoot(document.getElementById('root')).render(
