b40471b75e48ff8294078db6d91f30acbb21b618
huniiali1122@gmail.com <huniiali1122@gmail.com>
2025-12-02 20:43:56 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import SmoothScrolling from './hooks/SmoothScrolling.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Cursor from './components/Cursor.jsx'
 import Chilliman from './components/ChilliMan.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Loader from './components/Loader.jsx'
+import Cursor from './Components/Cursor.jsx'
 
 
 createRoot(document.getElementById('root')).render(
