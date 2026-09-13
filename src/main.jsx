4ef93029d22f8bca5e662dd114d3628aa0ede16e
huniiali1122@gmail.com <huniiali1122@gmail.com>
2026-09-14 03:31:12 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import Chilli from './Components/Chilli.jsx'
 import Mousemove from './Components/Mousemove.jsx'
 import Footer from './Components/Footer.jsx'
 import Header from './Components/Header.jsx'
-import Loader from './Components/Loader.jsx'
 import TechStack from './Sections/skills/TechStack.jsx'
 
 
@@ -17,7 +16,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SmoothScrolling>
       <BrowserRouter>
-      <Loader/>
+      {/* <Loader/> */}
       <Mousemove/>
       <Chilli/>
       {/* <Navbar/> */}
